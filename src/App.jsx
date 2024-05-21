6d1824c8ca23282c5589ab7995ff8b0c0fdb3334
LuizaMGama <luizamgama11@gmail.com>
2024-05-20 21:09:11 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+import Quiz from "./componentes/Quiz/Quiz";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Quiz />
+    </div>
+  );
+}
